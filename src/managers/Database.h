7e711630d25a95d8020d3f238bf40b9a14bd1301
André Moreira <moreiradev@hotmail.com>
2020-10-28 19:24:14 +0000

    Changed map to unordered map because of O(1) time complexity)

--- src/managers/Database.h
+++ src/managers/Database.h
@@ -9,21 +9,21 @@ class User;
 class Stream;
 
 #include <string>
-#include <map>
+#include <unordered_map>
 
 /**
  * Class to store the data from the application
  */
 class Database {
 private:
-    std::map<std::string, User *> users;
-    std::map<ID, Stream *> streams;
+    std::unordered_map<std::string, User *> users;
+    std::unordered_map<ID, Stream *> streams;
 public:
     Database() = default;
     /// @return A reference to the map containing the users
-    std::map<std::string, User *> & getUsers(){return users;};
+    std::unordered_map<std::string, User *> & getUsers(){return users;};
     /// @return A reference to the map containing the users
-    std::map<ID, Stream *> & getStreams() {return streams;};
+    std::unordered_map<ID, Stream *> & getStreams() {return streams;};
 };
 
 
