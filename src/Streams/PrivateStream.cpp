4286333b65eb5baa5d7b557108d80f14ae9486cc
André Moreira <moreiradev@hotmail.com>
2020-11-06 17:29:29 +0000

    Revert "fixed Exception"
    This reverts commit c00aac9cd673bd2ffb567515b0966804c58f4fc9.

--- src/Streams/PrivateStream.cpp
+++ src/Streams/PrivateStream.cpp
@@ -38,7 +38,7 @@ void PrivateStream::removeValidUser(const std::string &userNick) {
 
     auto it = find(whitelist.begin(),whitelist.end(),userNick);
     if(it == whitelist.end())
-        throw NotInWhiteListException(userNick,streamId);
+        throw NotInStreamException(userNick);
     else {
         whitelist.erase(it);
 
