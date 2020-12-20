6054f62764c1d8bb94fb8b0ac8bb8a3b8257cbb8
André Diogo Bastos Pereira <adbp@live.com.pt>
2020-12-20 19:29:51 +0000

    donation constructor

--- src/Users/Donation.h
+++ src/Users/Donation.h
@@ -1,13 +1,19 @@
 //
 // Created by adbp on 20/12/2020.
 //
-
 #ifndef FEUP_AEDA_PROJ_DONATION_H
 #define FEUP_AEDA_PROJ_DONATION_H
 
+#include <string>
+
 
 class Donation {
+public:
 
+private:
+    std::string streamerNick;
+    int amount;
+    int avaliation;
 };
 
 
