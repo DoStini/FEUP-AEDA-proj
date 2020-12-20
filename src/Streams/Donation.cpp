c842cdfcd2cb9475bf2907c447b24093c5c87a66
André Diogo Bastos Pereira <adbp@live.com.pt>
2020-12-20 19:21:48 +0000

    created dona

---
+++ src/Streams/Donation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adbp on 20/12/2020.
+//
+
+#include "Donation.h"
---
+++ src/Streams/Donation.h
@@ -0,0 +1,14 @@
+//
+// Created by adbp on 20/12/2020.
+//
+
+#ifndef FEUP_AEDA_PROJ_DONATION_H
+#define FEUP_AEDA_PROJ_DONATION_H
+
+
+class Donation {
+
+};
+
+
+#endif //FEUP_AEDA_PROJ_DONATION_H
